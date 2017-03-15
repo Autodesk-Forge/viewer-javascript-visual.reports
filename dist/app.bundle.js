99547d68c69a9f2ed76f5ab0bacd8fe3cad87080
shiya <luo.shiya@gmail.com>
2017-03-15 16:13:04 -0700

    fix pie chart size + theme tab

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -12420,7 +12420,28 @@
 
 	const themes = __webpack_require__(10);
 	const cameraPin = __webpack_require__(11);
+
+	var _lastPanel;
+
+	// resize panel vars
+	var isResizing = false,
+	    lastDownX = 0;
+
 	$(function() {
+
+	  $('.tab-buttons a').on('click', function(){
+	    $(this).closest('li').addClass('active').siblings().removeClass('active');
+	    $('.tab-panel').removeClass('active').filter(this.hash).addClass('active');
+
+	    if (_lastPanel === "tab_button_5")
+	    	cameraPin.uninitializePinPanel();
+
+	    if (this.id === "tab_button_5")
+	    	initializePinPanel();
+
+	    _lastPanel = this.id;
+	    return false
+	  });
 
 	  $('.report-select-input').change(function(){
 	    var theText = $(this).find('option:selected').text();
@@ -12431,6 +12452,9 @@
 	  });
 
 	});
+
+	// seems to be needed by Autodesk's viewer3D.js
+	function isTouchDevice() { return false; }
 
 
 /***/ }
